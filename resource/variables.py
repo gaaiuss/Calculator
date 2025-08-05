31755f681f19de5d257a3d4c798f93be2a283171
gaaiuss <caio.gui.castro@gmail.com>
2025-08-05 15:33:44 -0300

    sizing, color sections

--- resource/variables.py
+++ resource/variables.py
@@ -2,4 +2,17 @@ from pathlib import Path
 
 ROOT_DIR = Path(__file__).parent.parent
 FILES_DIR = ROOT_DIR / 'resource'
-WINDOW_ICON_PATH = FILES_DIR / 'icon.png'
+ICON_PATH = FILES_DIR / 'icon.png'
+
+# Sizing
+BIG_FONT_SIZE = 40
+MEDIUM_FONT_SIZE = 24
+SMALL_FONT_SIZE = 12
+TEXT_MARGIN = 10
+MINIMUM_WIDTH = 500
+
+# Colors
+DARKER_PRIMARY_COLOR = '#e97b9c'
+DARKEST_PRIMARY_COLOR = '#8f003c'
+PRIMARY_COLOR = '#440011'
+WHITE_COLOR = '#ffffff'
