a8757f43f60f03a957f6937bc09ebcc1e19dbb7a
gaaiuss <caio.gui.castro@gmail.com>
2025-08-05 15:35:18 -0300

    add comments and add layout func

--- gui/main_window.py
+++ gui/main_window.py
@@ -1,29 +1,30 @@
-from PySide6.QtWidgets import QMainWindow, QVBoxLayout, QWidget
+from resource.variables import ICON_PATH
+
 from PySide6.QtGui import QIcon
-from resource.variables import WINDOW_ICON_PATH
+from PySide6.QtWidgets import QMainWindow, QVBoxLayout, QWidget
 
 
 class MainWindow(QMainWindow):
     def __init__(self, parent: QWidget | None = None, *args, **kwargs) -> None:
         super().__init__(parent, *args, **kwargs)
 
-        # Configurando o layout básico
+        # Basic Layout
         self.cw = QWidget()
-        self.v_layout = QVBoxLayout()
-        self.cw.setLayout(self.v_layout)
+        self.vLayout = QVBoxLayout()
+        self.cw.setLayout(self.vLayout)
         self.setCentralWidget(self.cw)
 
-        # Título da janela
-        self.setWindowTitle('Calculadora')
+        # Window title
+        self.setWindowTitle('Calculator')
 
-        # Define o ícone
-        icon = QIcon(str(WINDOW_ICON_PATH))
+        # Window icon
+        icon = QIcon(str(ICON_PATH))
         self.setWindowIcon(icon)
 
     def adjustFixedSize(self):
-        # Última coisa a ser feita
+        # Last config to be done
         self.adjustSize()
         self.setFixedSize(self.width(), self.height())
 
-    def addWidgetToVLayout(self, widget: QWidget):
-        self.v_layout.addWidget(widget)
+    def addToLayout(self, widget: QWidget):
+        self.vLayout.addWidget(widget)
