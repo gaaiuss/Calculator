6dc616d338de3596628ef5ea22e0c3650f72f987
gaaiuss <caio.gui.castro@gmail.com>
2025-08-06 15:37:42 -0300

    input signals

--- gui/display.py
+++ gui/display.py
@@ -1,10 +1,18 @@
+from resource.utils import isEmpty, isNumOrDot
 from resource.variables import BIG_FONT_SIZE, MINIMUM_WIDTH, TEXT_MARGIN
 
-from PySide6.QtCore import Qt
+from PySide6.QtCore import Qt, Signal
+from PySide6.QtGui import QKeyEvent
 from PySide6.QtWidgets import QLineEdit
 
 
 class Display(QLineEdit):
+    eqPressed = Signal()
+    delPressed = Signal()
+    clearPressed = Signal()
+    inputPressed = Signal(str)
+    operatorPressed = Signal(str)
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.configStyle()
@@ -16,3 +24,41 @@ class Display(QLineEdit):
         self.setMinimumWidth(MINIMUM_WIDTH)
         self.setAlignment(Qt.AlignmentFlag.AlignRight)
         self.setTextMargins(*margins)
+
+    def keyPressEvent(self, event: QKeyEvent):
+        text = event.text().strip()
+        key = event.key()
+        KEYS = Qt.Key
+
+        isEnter = key in [KEYS.Key_Enter, KEYS.Key_Return, KEYS.Key_Equal]
+        isDelete = key in [KEYS.Key_Delete, KEYS.Key_Backspace]
+        isEsc = key in [KEYS.Key_Escape, KEYS.Key_C]
+        isOperator = key in [
+            KEYS.Key_Slash, KEYS.Key_Plus, KEYS.Key_Minus, KEYS.Key_Asterisk,
+            KEYS.Key_P
+        ]
+
+        if isEnter:
+            self.eqPressed.emit()
+            return event.ignore()
+
+        if isDelete:
+            self.delPressed.emit()
+            return event.ignore()
+
+        if isEsc:
+            self.clearPressed.emit()
+            return event.ignore()
+
+        if isOperator:
+            if text.lower() == 'p':
+                text = '^'
+            self.operatorPressed.emit(text)
+            return event.ignore()
+
+        if isEmpty(text):
+            return event.ignore()
+
+        if isNumOrDot(text):
+            self.inputPressed.emit(text)
+            return event.ignore()
