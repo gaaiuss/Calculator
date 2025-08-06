178afa0f72b241a6c9bd4e82192ba5aeb10963a5
gaaiuss <caio.gui.castro@gmail.com>
2025-08-06 15:38:14 -0300

    number colors

--- gui/styles.py
+++ gui/styles.py
@@ -1,22 +1,36 @@
-from resource.variables import (DARKER_PRIMARY_COLOR, DARKEST_PRIMARY_COLOR,
-                                PRIMARY_COLOR, WHITE_COLOR)
+from resource.variables import (DARKER_PRIMARY_COLOR, DARKER_SECONDARY_COLOR,
+                                DARKEST_PRIMARY_COLOR, DARKEST_SECONDARY_COLOR,
+                                PRIMARY_COLOR, SECONDARY_COLOR, WHITE_COLOR)
 
 import qdarkstyle  # type: ignore
 from PySide6.QtWidgets import QApplication
 
 qss = f"""
-    PushButton[cssClass="specialButton"] {{
+    QPushButton[cssClass="specialButton"] {{
         color: {WHITE_COLOR};
         background: {PRIMARY_COLOR};
         border-radius: 5px;
     }}
-    PushButton[cssClass="specialButton"]:hover {{
+    QPushButton[cssClass="specialButton"]:hover {{
         color: {WHITE_COLOR};
         background: {DARKER_PRIMARY_COLOR};
     }}
-    PushButton[cssClass="specialButton"]:pressed {{
+    QPushButton[cssClass="specialButton"]:pressed {{
         color: {WHITE_COLOR};
         background: {DARKEST_PRIMARY_COLOR};
+    }}
+    QPushButton[cssClass="normalButton"] {{
+        color: {WHITE_COLOR};
+        background: {SECONDARY_COLOR};
+        border-radius: 5px;
+    }}
+    QPushButton[cssClass="normalButton"]:hover {{
+        color: {WHITE_COLOR};
+        background: {DARKER_SECONDARY_COLOR};
+    }}
+    QPushButton[cssClass="normalButton"]:pressed {{
+        color: {WHITE_COLOR};
+        background: {DARKEST_SECONDARY_COLOR};
     }}
 """
 
